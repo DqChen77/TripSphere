b087dbcc0690fec6262e48a314059a795b4dd545
SenyuXie <senyuxie@qq.com>
2026-03-04 22:22:05 +0800

    fix: Update PostgreSQL username from 'root' to 'postgres' across multiple services and adjust initialization script to create user_db

--- infra/postgres/init.sql
+++ infra/postgres/init.sql
@@ -1,10 +1,5 @@
 -- PostgreSQL initialization script
--- user_db is created automatically via POSTGRES_DB env var.
--- Create additional databases for other services.
 
+CREATE DATABASE user_db;
 CREATE DATABASE order_db;
 CREATE DATABASE inventory_db;
-
--- Grant all privileges to root user
-GRANT ALL PRIVILEGES ON DATABASE order_db TO root;
-GRANT ALL PRIVILEGES ON DATABASE inventory_db TO root;
